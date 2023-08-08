004479226eb678a6d44404a412a802fdf1f055c2
Yan Amarante <yanlimaamarantec@hotmail.com>
2023-08-08 17:45:09 -0300

    adicionado: imagem e circulo da home responsivos

--- src/Pages/HomeScreen/HomeScreen.tsx
+++ src/Pages/HomeScreen/HomeScreen.tsx
@@ -12,14 +12,21 @@ function HomeScreen() {
 
     return (
         <div className="container-homeScreen">
-            <h1 className="titulo-homeScreen">Bem-vindo a nossa loja de skins</h1>
-            <p className="texto-homeScreen">
-                estamos empenhados em oferecer a melhor experiência de compra possível. Queremos ser sua escolha confiável para encontrar as skins dos seus sonhos e elevar sua experiência
-            </p>
-            <div className="imagem-containe-homeScreen">
-            <img className="imagem-homeScreen" src="https://imgur.com/6HFsviD.png" />
+            <div>
+            <div className="titulo-texto-grupo-homeScreen">
+                <h1 className="titulo-homeScreen">Bem-vindo a nossa loja de skins</h1>
+                <p className="texto-homeScreen">
+                    estamos empenhados em oferecer a melhor experiência de compra possível. Queremos ser sua escolha confiável para encontrar as skins dos seus sonhos e elevar sua experiência
+                </p>
+            </div>
+            <div className="imagem-container-homeScreen">
+                <div className="circulo-homeScreen">
+                </div>
+                    <img className="imagem-homeScreen" src="https://imgur.com/6HFsviD.png" />
+
             </div>
             <button className="botao-ofertas-homeScreen"><p className="texto-botao-homeScreen">Ofertas</p></button>
+        </div>
         </div>
     )
 }
