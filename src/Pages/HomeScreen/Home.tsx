60f83dc6ad77c89bb7b07efb7777ea986377671d
Yan Amarante <yanlimaamarantec@hotmail.com>
2023-08-18 23:41:00 -0300

    estilos da home muadados

--- src/Pages/HomeScreen/Home.tsx
+++ src/Pages/HomeScreen/Home.tsx
@@ -25,31 +25,39 @@ function Home() {
     }
 
     const gridSkins = skinsGrid.map(skin =>
-        <ul className="lista-skins-home" key={skin.id}>
-            <li className="lista-infos-home">
-                <img className="imagem-skin-home" src={skin.imagem} />
-                <p className="nome-skin-lista-home">{skin.nome}</p>
+        <ul className="grid-skins-lista-home" key={skin.id}>
+            <li className="grid-skins-infos-home">
+                <img className="grid-skins-imagem-home" src={skin.imagem} />
             </li>
         </ul>
     )
 
     return (
         <main className="container-home">
-            <article className="titulo-descricao-container">
-                <h1 className="titulo-home">Bem-vindo a nossa</h1>
-                <p className="descricao-home">
-                    estamos empenhados em oferecer a melhor experiência de compra possível. Queremos ser sua escolha confiável para encontrar as skins dos seus sonhos e elevar sua experiência
-                </p>
-                <button className="botao-ofertas1366-home"><label className="botao-texto-home">Ofertas</label></button>
-            </article>
-            <section className="lista-ofertas-home">
-                <section className="lista-container-home">
+            <section className="bem-vindo-container-home">
+                <article className="bem-vindo-texto-home">
+                    <h2 className="bem-vindo-titulo-home">Bem-vindo a nossa loja de skins</h2>
+                    <p className="bem-vindo-descricao-home">
+                        estamos empenhados em oferecer a melhor experiência de compra possível. Queremos ser sua escolha confiável para encontrar as skins dos seus sonhos e elevar sua experiência
+                    </p>
+                </article>
+                <section className="imagem-container-home">
+                    <img className="imagem-home" src="https://imgur.com/FpvtQXS.png" />
+                    <div className="retangulo-home"></div>
+                </section>
+            </section>
+            <section className="variedade-container-home">
+                <section className="grid-skins-variedade-home">
                     {gridSkins}
                 </section>
-                <p className="ofertas-cadastradas-home">{ofertasCadastradas}</p>
-            </section>
-            <section className="botao-container-home">
-                <button className="botao-ofertas-home"><label className="botao-texto-home">Ofertas</label></button>
+                <article className="variedade-texto-home">
+                    <h2 className="variedade-titulo-home">Variedade Excepcional</h2>
+                    <p className="variedade-descricao-home"> Nossa vasta seleção abrange desde skins clássicas até as mais recentes e cobiçadas. Seja qual for o seu estilo, temos algo para você.</p>
+                    <p className="variedade-ofertas-cadastradas-home">{ofertasCadastradas}</p>
+                </article>
+                <section className="grid-skins-variedade-home">
+                    {gridSkins}
+                </section>
             </section>
         </main>
     )
