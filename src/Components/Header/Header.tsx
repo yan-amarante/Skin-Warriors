16fb86b0cf36bc5e5359a3e0b81754a80c5c98c8
Yan Amarante <yanlimaamarantec@hotmail.com>
2023-12-26 19:23:19 -0300

    feat: add animations to the sideBar and the header logo

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -22,15 +22,7 @@ function Header() {
 
     }
 
-    function verifyMenuState() {
-
-        if (menu) return <SideMenu updateState={setMenu} />
-
-        else return
-
-    }
-
-    function changeOpacity() {
+    function changeMenuIconOpacity() {
 
         if (menu) return "menu-disabled"
 
@@ -41,8 +33,8 @@ function Header() {
     return (
 
         <header className="container-header">
-            {verifyMenuState()}
-            <Menu onClick={() => toggleMenu()} className={changeOpacity()} />
+            <SideMenu state={menu} updateState={setMenu} />
+            <Menu onClick={() => toggleMenu()} className={changeMenuIconOpacity()} />
             <SiteLogo />
             <img className="cart-icon" src={shoppingCart} alt="" />
         </header>
@@ -56,8 +48,8 @@ function SiteLogo() {
     return (
         <Link className="site-logo" to="/">
             <svg width="33" height="42" viewBox="0 0 33 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 0H4.35849L13.0755 22.0889H20.5472L28.6415 0H33V38.2667L23.6604 42V35.7778L27.3962 32.9778H19.3019V36.7111H14.3208V32.9778H9.96226H6.22642L9.96226 35.7778V42L0 38.2667V0Z" fill="#ffffff" />
-                <path d="M14.3208 0V20.5333H19.3019V0H14.3208Z" fill="#ffffff" />
+                <path d="M0 0H4.35849L13.0755 22.0889H20.5472L28.6415 0H33V38.2667L23.6604 42V35.7778L27.3962 32.9778H19.3019V36.7111H14.3208V32.9778H9.96226H6.22642L9.96226 35.7778V42L0 38.2667V0Z" fill="currentColor" />
+                <path d="M14.3208 0V20.5333H19.3019V0H14.3208Z" fill="currentColor" />
             </svg>
         </Link>
 
--- src/Components/SideMenu/index.tsx
+++ src/Components/SideMenu/index.tsx
@@ -5,16 +5,25 @@ import Close from "../Icons/Close"
 
 type SideMenuProps = {
 
+    state: boolean;
+
     updateState: React.Dispatch<React.SetStateAction<boolean>>;
 
 }
 
 
-function SideMenu({ updateState }: SideMenuProps) {
+function SideMenu({ state, updateState }: SideMenuProps) {
+
+    function verifySideMenuState() {
+
+        if (state) return "transition-side_menu"
+
+    }
+
 
     return (
 
-        <section className="menu-container">
+        <section className={`menu-container ${verifySideMenuState()}`}>
             <Close className="close-menu" menuState={updateState} />
         </section>
 
